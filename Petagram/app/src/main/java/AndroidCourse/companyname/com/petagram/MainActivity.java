c5988d011f636ae8efa3e3f7b99d4cb4402524df
Guillermo <memo_inge@hotmail.com>
2019-03-11 00:09:41 -0600

    Proyecto

---
+++ Petagram/app/src/main/java/AndroidCourse/companyname/com/petagram/MainActivity.java
@@ -0,0 +1,13 @@
+package AndroidCourse.companyname.com.petagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
